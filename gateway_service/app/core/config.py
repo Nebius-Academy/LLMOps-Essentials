fb1dc0e6b01a02b2c508189c433f52ffc4f3ce73
Stanislav Fedotov <st-fedotov@ai-dt.school>
2024-12-10 16:22:00 +0000

    Update config.py

--- gateway_service/app/core/config.py
+++ gateway_service/app/core/config.py
@@ -8,3 +8,4 @@ INFERENCE_ROUTING = {"gpt-4o-mini": "gpt-4-mini", "gpt-4o": "gpt-4o",
                      "meta-llama/Meta-Llama-3.1-405B-Instruct": "llama-405B",
                      "meta-llama/Meta-Llama-3.1-70B-Instruct": "llama-70B", 
                      "meta-llama/Meta-Llama-3.1-8B-Instruct": "llama-8B"}
+
