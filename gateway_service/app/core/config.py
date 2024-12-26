6ba920650524af1efb18d7fc1d141742242c039f
Stanislav Fedotov <st-fedotov@ai-dt.school>
2024-12-26 15:32:36 +0000

    Update config.py

--- gateway_service/app/core/config.py
+++ gateway_service/app/core/config.py
@@ -4,4 +4,7 @@ DATABASE_URL = "postgresql://user:password@postgres/dbname"
 ADMIN_TOKEN = os.getenv("ADMIN_KEY")
 RATE_LIMIT = 5  # max requests per minute
 RATE_LIMIT_KEY_PREFIX = "rate_limit"
-INFERENCE_ROUTING = {"gpt-4o-mini": "gpt-4-mini", "gpt-4o": "gpt-4o"}
+INFERENCE_ROUTING = {"gpt-4o-mini": "gpt-4-mini", "gpt-4o": "gpt-4o",
+                     "meta-llama/Meta-Llama-3.1-405B-Instruct": "llama-405B",
+                     "meta-llama/Meta-Llama-3.1-70B-Instruct": "llama-70B", 
+                     "meta-llama/Meta-Llama-3.1-8B-Instruct": "llama-8B"}
